b88dc765d51012fd680d67bde45b25998c703079
kapilsupekar21 <kapilsuekar21@gmail.com>
2024-05-06 15:15:26 +0530

    Done

---
+++ src/main/java/NewCalss.java
@@ -0,0 +1,13 @@
+
+public class NewCalss {
+
+	public static void main(String[] args) {
+		System.out.println("System.out.println(\"Hello\");Hello");
+			
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
